d5066bd295e7710d76a97026f30b301c75a309b9
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-06-28 13:54:50 +0530

    Understood file structures

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	my_package "mylearning/my_package"
+)
 
 func main() {
 	fmt.Println("Learn GO");
+	my_package.PrintMessage("File structure testing")
 }
