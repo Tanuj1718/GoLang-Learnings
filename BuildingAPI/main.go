1720df860d275cc56940b0589cf2aa0105b2fe80
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-07-09 21:45:22 +0530

    Build API

---
+++ BuildingAPI/main.go
@@ -0,0 +1,128 @@
+package main
+
+//encoder for sending data to the request body
+//decoder for extracting data from the request
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// model for course  -file
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	FullName string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+//fake DB
+var courses []Course
+
+//middleware, helper -separate file
+func (c *Course) IsEmpty() bool {
+	return c.CourseName==""
+}
+
+func main() {
+	fmt.Println("Building API...")
+	
+}
+
+//controllers -separate file
+
+// serve home route i.e making a home route
+func serveHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Welcome to API Building</h1>"))
+}
+
+//one more route
+func getAllCourses(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get all Courses...")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses) //whatever value is passed in encode, it will get treated as json value and will be thrown back to whoever making this request
+}
+
+
+func GetOneCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get One Course...")
+	w.Header().Set("Content-Type", "application/json")
+
+	//grab course id from request
+	params := mux.Vars(r)
+	fmt.Println(params)
+
+	//loop through courses, find matching id that user has sent, return the response
+	for _, course := range courses{
+		if(course.CourseId==params["id"]){
+			json.NewEncoder(w).Encode(course)
+			return 
+		}
+
+	}
+	json.NewEncoder(w).Encode("No course found with given id")
+	return
+}
+
+
+func CreateOneCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Create one course...")
+	w.Header().Set("Content-Type", "application/json")
+
+	//what if: body is empty
+	if(r.Body==nil){
+		json.NewEncoder(w).Encode("Please send some data")
+		return
+	}
+
+	//what about data is send like this - {}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+
+	if(course.IsEmpty()){
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return 
+	}
+
+	//generate unique id, convert into string
+	//append course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+}
+
+func UpdateOneCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Updating one course...")
+	w.Header().Set("Content-Type", "application/json")
+
+	//first -grab id from request
+	params := mux.Vars(r)
+
+	//loop to find id, then remove, then add with my id(new id which is in params)
+	for index, course := range courses{
+		if(course.CourseId==params["id"]){
+			courses = append(courses[:index], courses[index+1:]...) //removed the previous course
+			var newcourse Course
+			_ = json.NewDecoder(r.Body).Decode(&newcourse)
+			newcourse.CourseId = params["id"]
+			courses = append(courses, newcourse)
+			json.NewEncoder(w).Encode(newcourse)
+			return
+		}
+	}
+}
--- practice/main.go
+++ practice/main.go
@@ -7,29 +7,29 @@ import (
 	"net/http"
 )
 
-type Problem struct{
+type Problem struct {
 	Name string `json:"name"`
 }
 
-type Member struct{
+type Member struct {
 	Handle string `json:"handle"`
 }
 
-type Author struct{
+type Author struct {
 	Members []Member `json:"members"`
 }
 
-type Cheater struct{
-	CandidateId int `json:"id"`
-	CandidateName Author `json:"author"`
-	ContestId int `json:"contestId"`
-	ProblemName Problem `json:"problem"`
-	Verdict string `json:"verdict"`
+type Cheater struct {
+	CandidateId   int     `json:"id"`
+	CandidateName Author  `json:"author"`
+	ContestId     int     `json:"contestId"`
+	ProblemName   Problem `json:"problem"`
+	Verdict       string  `json:"verdict"`
 }
 
-type Apiresponse struct{
-	Status string 
-	Result []Cheater 
+type Apiresponse struct {
+	Status string
+	Result []Cheater
 }
 
 func main() {
@@ -37,11 +37,14 @@ func main() {
 	getresponse()
 }
 
-func getresponse(){
-	myurl := "https://codeforces.com/api/user.status?handle=Benq&from=1&count=20"
+func getresponse() {
+	fmt.Printf("Enter CF id: ")
+	var id string
+	fmt.Scan(&id)
+	myurl := fmt.Sprintf("https://codeforces.com/api/user.status?handle=%s&from=1&count=20", id)
 
 	response, err := http.Get(myurl)
-	if(err!=nil){
+	if err != nil {
 		panic(err)
 	}
 
@@ -49,15 +52,18 @@ func getresponse(){
 
 	data, err := ioutil.ReadAll(response.Body)
 
-	
 	var apiresponse Apiresponse
 	err = json.Unmarshal(data, &apiresponse)
-	if(err!=nil){
+	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Candidate Name: %s\n", apiresponse.Result[1].CandidateName.Members[0].Handle)
 
-	for _, it := range apiresponse.Result{
+	for _, it := range apiresponse.Result {
+		if it.Verdict == "SKIPPED" {
+			fmt.Println("Candidate is a cheater")
+			break
+		}
 		fmt.Printf("\nCandidateID: %d\n ContestID: %d\n Problem: %s\n Verdict: %s\n", it.CandidateId, it.ContestId, it.ProblemName.Name, it.Verdict)
 	}
 
